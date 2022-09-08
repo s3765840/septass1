2ee9230a1b29ad4e07f18ee22d4e70b837eb5b7c
s3765840@student.rmit.edu.au <61450190+Huili233@users.noreply.github.com>
2022-09-08 22:02:47 +1000

    first commit

---
+++ src/test/java/com/example/septass1/Septass1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.septass1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Septass1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
